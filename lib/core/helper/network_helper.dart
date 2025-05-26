a09f529e6572af36a07061ae327f755f35164371
JaeHwan <wpghks56@naver.com>
2025-05-26 16:52:08 +0900

    [Add] dio 요청마다 api key 보내도록 설정

--- lib/core/helper/network_helper.dart
+++ lib/core/helper/network_helper.dart
@@ -1,13 +1,21 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 abstract class NetworkHelper {
   static final Dio dio =
       Dio()
         ..interceptors.add(
           InterceptorsWrapper(
-            onRequest: (options, handler) {},
-            onResponse: (response, handler) {},
-            onError: (DioException e, handler) {},
+            onRequest: (options, handler) {
+              return handler.next(options);
+            },
+            onResponse: (response, handler) {
+              return handler.next(response);
+            },
+            onError: (DioException e, handler) {
+              return handler.next(e);
+            },
           ),
-        );
+        )
+        ..options.queryParameters['api_key'] = dotenv.env['LASTFM_KEY'];
 }
