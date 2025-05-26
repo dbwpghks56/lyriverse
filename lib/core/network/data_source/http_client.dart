400872c32de4345a1d5d04c5bc345f486b441a1b
JaeHwan <wpghks56@naver.com>
2025-05-26 22:20:03 +0900

    [Add] pagination 추가

--- lib/core/network/data_source/http_client.dart
+++ lib/core/network/data_source/http_client.dart
@@ -1,4 +1,6 @@
 import 'package:lyriverse/core/network/models/http_response.dart';
+import 'package:lyriverse/core/network/pagination/paginated_response.dart';
+import 'package:lyriverse/core/network/pagination/pagination_request.dart';
 
 abstract interface class HttpClient {
   Future<HttpResponse<T>> get<T>(
@@ -38,5 +40,17 @@ abstract interface class HttpClient {
     Map<String, dynamic>? queryParameters,
     Map<String, String>? headers,
     Duration? timeout,
+  });
+
+  // Pagination 지원 메서드들
+  Future<HttpResponse<PaginatedResponse<T>>> getPaginated<T>(
+    String path, {
+    required PaginationRequest pagination,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+    Duration? timeout,
+    required T Function(Map<String, dynamic>) itemParser,
+    required PaginatedResponse<T> Function(Map<String, dynamic>, List<T>)
+    responseParser,
   });
 }
