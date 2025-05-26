11672cfb060422005405c248c74cb555314487eb
JaeHwan <wpghks56@naver.com>
2025-05-26 22:19:33 +0900

    [Fix] import 변경

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -1,9 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:lyriverse/core/helper/network_helper.dart';
-import 'package:lyriverse/core/network/data_source/dio_http_client.dart';
+import 'package:lyriverse/core/network/data_source/impl/dio_http_client.dart';
 import 'package:lyriverse/core/network/data_source/http_client.dart';
-import 'package:lyriverse/track/data/data_source/remote_track_data_source_impl.dart';
+import 'package:lyriverse/track/data/data_source/impl/remote_track_data_source_impl.dart';
 import 'package:lyriverse/track/data/data_source/track_data_source.dart';
 
 final getIt = GetIt.instance;
