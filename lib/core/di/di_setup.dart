765188767fe19778d452310f9c5cde1d0698a541
JaeHwan <wpghks56@naver.com>
2025-05-27 21:16:40 +0900

    [Add] lyric 관련 di 추가

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -3,6 +3,10 @@ import 'package:get_it/get_it.dart';
 import 'package:lyriverse/core/helper/network_helper.dart';
 import 'package:lyriverse/core/network/data_source/impl/dio_http_client.dart';
 import 'package:lyriverse/core/network/data_source/http_client.dart';
+import 'package:lyriverse/lyric/data/data_source/impl/remote_lyric_data_source.dart';
+import 'package:lyriverse/lyric/data/data_source/lyric_data_source.dart';
+import 'package:lyriverse/lyric/data/repository/lyric_repository_impl.dart';
+import 'package:lyriverse/lyric/domain/repository/lyric_repository.dart';
 import 'package:lyriverse/track/data/data_source/impl/remote_track_data_source_impl.dart';
 import 'package:lyriverse/track/data/data_source/track_data_source.dart';
 import 'package:lyriverse/track/data/repository/track_repository_impl.dart';
@@ -14,6 +18,10 @@ void di() {
   getIt.registerSingleton<String>(
     'https://ws.audioscrobbler.com/2.0/',
     instanceName: 'lastFmUrl',
+  );
+  getIt.registerSingleton<String>(
+    'https://lrclib.net/api/get',
+    instanceName: 'lyricUrl',
   );
   getIt.registerLazySingleton<Dio>(() => NetworkHelper.dio);
   getIt.registerLazySingleton<HttpClient>(
@@ -29,5 +37,16 @@ void di() {
   );
   getIt.registerLazySingleton<TrackRepository>(
     () => TrackRepositoryImpl(dataSource: getIt<TrackDataSource>()),
+  );
+
+  // Lyric DI
+  getIt.registerLazySingleton<LyricDataSource>(
+    () => RemoteLyricDataSource(
+      httpClient: getIt<HttpClient>(),
+      remoteUrl: getIt<String>(instanceName: 'lyricUrl'),
+    ),
+  );
+  getIt.registerLazySingleton<LyricRepository>(
+    () => LyricRepositoryImpl(dataSource: getIt<LyricDataSource>()),
   );
 }
