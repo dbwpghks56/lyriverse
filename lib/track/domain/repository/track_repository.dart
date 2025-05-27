1446437098bfe3d1263e1996d778688ad7b05b33
JaeHwan <wpghks56@naver.com>
2025-05-27 20:31:01 +0900

    [Add] repository 에 track 찾아오는 서비스 로직 추가

--- lib/track/domain/repository/track_repository.dart
+++ lib/track/domain/repository/track_repository.dart
@@ -1,1 +1,11 @@
-abstract interface class TrackRepository {}
+import 'package:lyriverse/core/network/pagination/paginated_response.dart';
+import 'package:lyriverse/core/network/pagination/pagination_request.dart';
+import 'package:lyriverse/track/domain/model/track.dart';
+
+abstract interface class TrackRepository {
+  Future<PaginatedResponse<Track>> findTracksByArtistAndName({
+    required String track,
+    String? artist,
+    required PaginationRequest pagination,
+  });
+}
