8a3aa1b9a4886eaed4dd9e7b318bee713202fa56
JaeHwan <wpghks56@naver.com>
2025-06-30 16:49:44 +0900

    [refactor] artist model dto mapper refactor

--- lib/artist/data/mapper/artist_mapper.dart
+++ lib/artist/data/mapper/artist_mapper.dart
@@ -3,12 +3,22 @@ import 'package:lyriverse/artist/domain/model/artist.dart';
 
 extension ArtistDtoMapper on ArtistDto {
   Artist toModel() {
-    return Artist(name: name ?? 'N/A', url: url ?? 'N/A');
+    return Artist(
+      id: id ?? 'N/A',
+      name: name ?? 'N/A',
+      href: href ?? 'N/A',
+      type: type ?? 'N/A',
+    );
   }
 }
 
 extension ArtistMapper on Artist {
   ArtistDto toDto() {
-    return ArtistDto(name: name, url: url);
+    return ArtistDto(
+      id: id,
+      name: name,
+      href: href,
+      type: type,
+    );
   }
 }
--- lib/artist/domain/model/artist.dart
+++ lib/artist/domain/model/artist.dart
@@ -4,13 +4,16 @@ part 'artist.freezed.dart';
 
 @freezed
 class Artist with _$Artist {
+  final String id;
+  final String href;
   @override
   final String name;
-  @override
-  final String url;
+  final String type;
 
   const Artist({
+    required this.id,
+    required this.href,
     required this.name,
-    required this.url,
+    required this.type,
   });
 }
